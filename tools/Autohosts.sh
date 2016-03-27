1a61da66d54d3675b097daa0be25d61ef77bc98d
wengmingao <wengmingao@qq.com>
2016-03-27 13:59:58 +0800

    增加判断
    马上要回学校了...改的比较匆忙

--- tools/Autohosts.sh
+++ tools/Autohosts.sh
@@ -45,26 +45,28 @@ for i in `seq 3`; do
 	fi
 done
 
-echo '尝试从Sinaapp镜像中获取'
-for i in `seq 3`; do
-	wget -N $sinapp -o outlog #下载并保存记录到outlog
-	stage=`grep -c 'not retrieving' outlog` #1.16版本兼容 无304 而是not retrieving
-	if [ $stage == 0 ]; then
-		stage=`grep -c '200' outlog`
+if [ $stage == 0 ]; then
+	echo '尝试从Sinaapp镜像中获取'
+	for i in `seq 3`; do
+		wget -N $sinapp -o outlog #下载并保存记录到outlog
+		stage=`grep -c 'not retrieving' outlog` #1.16版本兼容 无304 而是not retrieving
 		if [ $stage == 0 ]; then
-			echo "获取失败第 $i 次"
-			sleep 5
+			stage=`grep -c '200' outlog`
+			if [ $stage == 0 ]; then
+				echo "获取失败第 $i 次"
+				sleep 5
+			else
+				echo '从Sinaapp中获取成功'
+				echo '# Response: 200[Sinaapp Updated]'>log
+				break
+			fi
 		else
 			echo '从Sinaapp中获取成功'
-			echo '# Response: 200[Sinaapp Updated]'>log
+			echo '# Response: 304[Sinaapp NoUpdated]'>log
 			break
 		fi
-	else
-		echo '从Sinaapp中获取成功'
-		echo '# Response: 304[Sinaapp NoUpdated]'>log
-		break
-	fi
-done
+	done
+fi
 
 if [ $stage == 0 ]; then
 	echo '从Sinaapp中获取失败'
