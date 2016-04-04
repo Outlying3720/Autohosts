28a646a9b48f663f557b0515cd46b0eacbe25e6c
wengmingao <wengmingao@qq.com>
2016-04-04 20:56:19 +0800

    调整文件保存位置
    调整了文件保存位置,取消了index.html改用列目录方式排查更新
    将outlog改名为outlog.log使其被浏览器认作text

--- tools/Autohosts.sh
+++ tools/Autohosts.sh
@@ -14,7 +14,7 @@ for i in `seq 15`; do
 done
 
 #定义下载目录
-path='/www/hosts'
+path='/www/app/hosts'
 cd $path
 echo "当前下载目录 $path"
 
@@ -26,10 +26,10 @@ github='https://raw.githubusercontent.com/racaljk/hosts/master/hosts'
 #下载部分
 echo '尝试从rhcloud镜像中获取'
 for i in `seq 3`; do
-	wget --no-check-certificate -N $rhcloud -o outlog #下载并保存记录到outlog
-	stage=`grep -c 'not retrieving' outlog` #1.16版本兼容 无304 而是not retrieving
+	wget --no-check-certificate -N $rhcloud -o outlog.log #下载并保存记录到outlog.log
+	stage=`grep -c 'not retrieving' outlog.log` #1.16版本兼容 无304 而是not retrieving
 	if [ $stage == 0 ]; then
-		stage=`grep -c '200' outlog`
+		stage=`grep -c '200' outlog.log`
 		if [ $stage == 0 ]; then
 			echo "获取失败第 $i 次"
 			sleep 5
@@ -48,10 +48,10 @@ done
 if [ $stage == 0 ]; then
 	echo '尝试从Sinaapp镜像中获取'
 	for i in `seq 3`; do
-		wget -N $sinapp -o outlog #下载并保存记录到outlog
-		stage=`grep -c 'not retrieving' outlog` #1.16版本兼容 无304 而是not retrieving
+		wget -N $sinapp -o outlog.log #下载并保存记录到outlog.log
+		stage=`grep -c 'not retrieving' outlog.log` #1.16版本兼容 无304 而是not retrieving
 		if [ $stage == 0 ]; then
-			stage=`grep -c '200' outlog`
+			stage=`grep -c '200' outlog.log`
 			if [ $stage == 0 ]; then
 				echo "获取失败第 $i 次"
 				sleep 5
@@ -72,7 +72,7 @@ if [ $stage == 0 ]; then
 	echo '从Sinaapp中获取失败'
 	echo '尝试从Github中获取'
 	for i in `seq 3`; do
-		wget --no-check-certificate $github -o outlog -O hosts_new
+		wget --no-check-certificate $github -o outlog.log -O hosts_new
 		if [[ -s hosts_new ]]; then
 			echo '从Github中获取成功'
 			echo '# Response: 200[Github]'>log
